ffeb6bc4ef1dea4d4317ec81dd6aa866cdfbbfa6
carlosestrada22 <carlos.estradamercado@alumnos.udg.mx>
2017-12-26 18:44:18 -0600

    feat add jquery, footer, nav

--- carrier/src/App.js
+++ carrier/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import { Router, Route, Switch } from 'react-router'
 import './App.css';
+import 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/js/materialize.js';
+import 'materialize-css/js/cards.js';
+import 'materialize-css/js/animation.js';
+import 'materialize-css/js/dropdown.js';
+import 'materialize-css/js/sideNav.js';
+import $ from 'jquery'
+
+import Navbar from './components/navbar/navbar.js'
+import Footer from './components/footer/footer.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar $={$}/>
+        <div className="main"></div>
+        <Footer />
       </div>
     );
   }
