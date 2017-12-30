43c07c0b1000c4236e61b79da7b5fb7fc9a70dd1
carlosestrada22 <carlos.estradamercado@alumnos.udg.mx>
2017-12-30 00:56:08 -0600

    fixed greet size

--- carrier/src/components/greet/greet.js
+++ carrier/src/components/greet/greet.js
@@ -3,9 +3,11 @@ import React from 'react'
 const Greet = () => {
     return (
         <div className="container">
-            <h4 className="center-align with-subtitle">Greet homs</h4>
-            <h5 className="subtitle center-align">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi possimus quidem soluta fugit maiores minus optio, culpa alias necessitatibus! Eum praesentium ut a dolores saepe unde expedita eos dolore fugiat!</h5>
-            <img src="http://via.placeholder.com/1280x480" alt="logo" />
+            <div className="row">
+                <h4 className="center-align with-subtitle">Greet homs</h4>
+                <h5 className="subtitle center-align">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi possimus quidem soluta fugit maiores minus optio, culpa alias necessitatibus! Eum praesentium ut a dolores saepe unde expedita eos dolore fugiat!</h5>
+                <img src="http://via.placeholder.com/1280x480" width="100%" alt="logo" />
+            </div>
         </div>
     )
 }
