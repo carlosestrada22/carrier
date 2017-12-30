556345cb4cd076af168de82e2456ac2ae4fe5ae6
carlosestrada22 <carlos.estradamercado@alumnos.udg.mx>
2017-12-29 23:33:38 -0600

    feat add banner

---
+++ carrier/src/components/banner/banner.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './banner.css'
+
+const Banner = () => {
+    return (
+        <div className="pre-banner">
+            <div className="container">
+                <div className="row center no-margin">
+                    <div className="col s6 m2"><img src="http://via.placeholder.com/150x64" alt="logo" /></div>
+                    <div className="col s6 m2"><img src="http://via.placeholder.com/150x64" alt="logo" /></div>
+                    <div className="col s6 m2"><img src="http://via.placeholder.com/150x64" alt="logo" /></div>
+                    <div className="col s6 m2"><img src="http://via.placeholder.com/150x64" alt="logo" /></div>
+                    <div className="col s6 m2"><img src="http://via.placeholder.com/150x64" alt="logo" /></div>
+                    <div className="col s6 m2"><img src="http://via.placeholder.com/150x64" alt="logo" /></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
