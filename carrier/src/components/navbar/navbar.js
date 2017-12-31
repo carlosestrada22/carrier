acfc7f8969d1f8d78876df15278830d9f295d2b0
carlosestrada22 <carlos.estradamercado@alumnos.udg.mx>
2017-12-30 20:41:49 -0600

    style add Navlink instead of link to navbar

--- carrier/src/components/navbar/navbar.js
+++ carrier/src/components/navbar/navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './navbar.css'
 
 class Navbar extends Component {
@@ -15,26 +15,26 @@ class Navbar extends Component {
                 <nav>
                     <div className="nav-wrapper white ">
                         <div className="container navbar">
-                            <img src="http://via.placeholder.com/150x64" alt="logo" className="left responsive-img hide-on-med-and-down"/>
-                            {/* <Link to={`${this.state.match.url}/`} className="left brand-logo">LOGO</Link> */}
+                            <img src="http://via.placeholder.com/150x64" alt="logo" className="left responsive-img hide-on-med-and-down" />
+                            {/* <NavLink to={`${this.state.match.url}/`} className="left brand-logo">LOGO</NavLink> */}
                             <a href="#!" data-activates="mobile-demo" className="button-collapse"><i className="material-icons black-text">menu</i></a>
                             <ul className="right hide-on-med-and-down">
-                                <li><Link to={`${this.state.match.url}/`} >Home</Link></li>
-                                <li><Link to={`${this.state.match.url}/about-us`}>About us</Link></li>
-                                <li><Link to={`${this.state.match.url}/carriers`}>Carriers</Link></li>
-                                <li><Link to={`${this.state.match.url}/enterprise`}>Enterprise</Link></li>
-                                <li><Link to={`${this.state.match.url}/packages`}>Packages</Link></li>
-                                <li><Link to={`${this.state.match.url}/public-sector`}>Public sector</Link></li>
-                                <li><Link to={`${this.state.match.url}/contact-us`}>Contact us</Link></li>
+                                <li><NavLink to={`${this.state.match.url}/`} >Home</NavLink></li>
+                                <li><NavLink to={`${this.state.match.url}/about-us`} activeClassName='link-active'>About us</NavLink></li>
+                                <li><NavLink to={`${this.state.match.url}/carriers`} activeClassName='link-active'>Carriers</NavLink></li>
+                                <li><NavLink to={`${this.state.match.url}/enterprise`} activeClassName='link-active'>Enterprise</NavLink></li>
+                                <li><NavLink to={`${this.state.match.url}/packages`} activeClassName='link-active'>Packages</NavLink></li>
+                                <li><NavLink to={`${this.state.match.url}/public-sector`} activeClassName='link-active'>Public sector</NavLink></li>
+                                <li><NavLink to={`${this.state.match.url}/contact-us`} activeClassName='link-active'>Contact us</NavLink></li>
                             </ul>
                             <ul className="side-nav" id="mobile-demo">
-                                <li><Link to={`${this.state.match.url}/`}>Home</Link></li>
-                                <li><Link to={`${this.state.match.url}/about-us`}>About us</Link></li>
-                                <li><Link to={`${this.state.match.url}/carriers`}>Carriers</Link></li>
-                                <li><Link to={`${this.state.match.url}/enterprise`}>Enterprise</Link></li>
-                                <li><Link to={`${this.state.match.url}/packages`}>Packages</Link></li>
-                                <li><Link to={`${this.state.match.url}/public-sector`}>Public sector</Link></li>
-                                <li><Link to={`${this.state.match.url}/contact-us`}>Contact us</Link></li>
+                                <li><NavLink to={`${this.state.match.url}/`}>Home</NavLink></li>
+                                <li><NavLink to={`${this.state.match.url}/about-us`}>About us</NavLink></li>
+                                <li><NavLink to={`${this.state.match.url}/carriers`}>Carriers</NavLink></li>
+                                <li><NavLink to={`${this.state.match.url}/enterprise`}>Enterprise</NavLink></li>
+                                <li><NavLink to={`${this.state.match.url}/packages`}>Packages</NavLink></li>
+                                <li><NavLink to={`${this.state.match.url}/public-sector`}>Public sector</NavLink></li>
+                                <li><NavLink to={`${this.state.match.url}/contact-us`}>Contact us</NavLink></li>
                             </ul>
                         </div>
                     </div>
