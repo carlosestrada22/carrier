ac9fd95d148ce1a1290b427a1854e4f4f88f0a23
carlosestrada22 <carlos.estradamercado@alumnos.udg.mx>
2017-12-30 21:13:33 -0600

    feat adjust on slider's style

--- carrier/src/components/slider/slider.js
+++ carrier/src/components/slider/slider.js
@@ -3,24 +3,33 @@ import $ from 'jquery'
 
 class Slider extends Component {
     state = {}
-    componentDidMount = () => {
-        $('.carousel.carousel-slider').carousel({ fullWidth: true })
-        $(document).ready(function () {
-            $('.parallax').parallax();
-        });
-    }
+    componentDidMount = () => $('.carousel.carousel-slider').carousel({ fullWidth: true })
+
     render() {
         return (
             <div className="row">
-                <div className="carousel carousel-slider ">
-                    <a className="carousel-item" href="#one!"><img src="http://via.placeholder.com/1920x480" alt="" /></a>
-                    <a className="carousel-item" href="#two!"><img src="https://lorempixel.com/1920/1080/food/2" alt="" /></a>
-                    <a className="carousel-item" href="#three!"><img src="https://lorempixel.com/1920/1080/food/3" alt="" /></a>
-                    <a className="carousel-item" href="#four!"><img src="https://lorempixel.com/1920/1080/food/4" alt="" /></a>
+                <div className="carousel carousel-slider" data-indicators="true">
+                    <div className="carousel-fixed-item center">
+                        <a className="btn btn-large waves-effect  dark-primary-color white-text ">Botón <i className="material-icons right">shopping_cart</i></a>
+                    </div>
+                    {getSliders().map(src => <SliderItem src={src} />)}
                 </div>
             </div>
         );
     }
 }
 
+const getSliders = () => {
+    let arr = ["http://via.placeholder.com/1920x480"]
+    for (let i = 2; i < 5; i++)
+        arr.push(`https://lorempixel.com/1920/1080/food/${i}`)
+    return arr
+}
+
+const SliderItem = ({ src }) => {
+    return (
+        <a className="carousel-item" href="#!"><img src={src} alt={src.toString()} /></a>
+    )
+}
 export default Slider;
+
