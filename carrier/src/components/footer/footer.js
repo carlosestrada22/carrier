9507a207df2f7c3550aab3b719d3aded439066e7
carlosestrada22 <carlos.estradamercado@alumnos.udg.mx>
2017-12-30 21:21:11 -0600

    style footer

--- carrier/src/components/footer/footer.js
+++ carrier/src/components/footer/footer.js
@@ -9,23 +9,15 @@ class Footer extends Component {
                 <footer className="page-footer dark-primary-color">
                     <div className="container">
                         <div className="row">
-                            <div className="col l6 s12">
-                                <h5 className="white-text">Footer Content</h5>
-                                <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-                            </div>
-                            <div className="col l4 offset-l2 s12">
-                                <h5 className="white-text">Follow us!</h5>
-                                <ul className="follow-us-icons">
-                                    <li><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/F_icon.svg/2000px-F_icon.svg.png" width="50" alt="fb" /></li>
-                                    <li><img src="https://upload.wikimedia.org/wikipedia/fr/thumb/c/c8/Twitter_Bird.svg/1259px-Twitter_Bird.svg.png" width="50" alt="fb" /></li>
-                                </ul>
-                            </div>
+                            <Links />
+                            <Links />
+                            <Links />
+                            <Links />
                         </div>
                     </div>
                     <div className="footer-copyright">
                         <div className="container">
-                            © 2014 Copyright Text
-    <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+                            © 2018 Telecom
                         </div>
                     </div>
                 </footer>
@@ -33,6 +25,20 @@ class Footer extends Component {
             </div>
         )
     }
+}
+
+const Links = () => {
+    return (
+        <div className="col s12 m3">
+            <h5>Links</h5>
+            <ul>
+                <li>item 1</li>
+                <li>item 2</li>
+                <li>item 3</li>
+                <li>item 4</li>
+            </ul>
+        </div>
+    )
 }
 
 export default Footer
